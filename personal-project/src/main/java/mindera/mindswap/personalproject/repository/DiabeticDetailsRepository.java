cfe1fd12e52f52f8b49fe962b28aba4f879eb506
Gonçalo <goncaloscravo@gmail.com>
2023-08-29 22:40:49 +0100

    Add: user endpoint

---
+++ personal-project/src/main/java/mindera/mindswap/personalproject/repository/DiabeticDetailsRepository.java
@@ -0,0 +1,9 @@
+package mindera.mindswap.personalproject.repository;
+
+import mindera.mindswap.personalproject.model.diabeticDetails.DiabeticDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiabeticDetailsRepository extends JpaRepository<DiabeticDetails, Long> {
+}
